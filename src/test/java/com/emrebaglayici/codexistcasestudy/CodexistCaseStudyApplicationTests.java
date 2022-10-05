5a7acb0e403753d5bb1147b8754c791638d192ce
emre <emrebaglayici1@gmail.com>
2022-10-05 19:08:45 +0300

    Initializing the case study.

---
+++ src/test/java/com/emrebaglayici/codexistcasestudy/CodexistCaseStudyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.emrebaglayici.codexistcasestudy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodexistCaseStudyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
